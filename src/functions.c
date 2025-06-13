1e2493ef590e836cd29130b10ed36f190ae558b1
ADEL-Beljohol <beljohol.adel@etu.estia.fr>
2025-06-13 15:18:42 +0200

    Fonction Max_pixel V1

--- src/functions.c
+++ src/functions.c
@@ -8,4 +8,33 @@ void min_component(char *image) {
     
 }
 
-void max_pixel(char*image){}
+void max_pixel(char *image_path) {
+    int x,y;
+    int l,L,c;
+    read_image_data(image_path, %l, %L, %c);
+
+    int somme;
+    int i;
+    int r, g, b;
+
+    int lol;  
+    int max_somme = 0;
+    int max_i = 0;
+
+    for (i=0; i<l*L;i++){
+        lol = i*c;
+        r = data[lol];
+        g = data[lol +1];
+        b = data[lol +2];
+        somme = r+g+b;
+
+        if (somme>max_somme){
+            max_somme = somme;
+            max_i = lol;
+        }
+    }
+
+    x = max_index;
+    y = max_index;
+    printf("max_pixel(%d, %d): %d, %d, %d\n",x,y,data[id],data[id+1],data[id+2]);
+}
