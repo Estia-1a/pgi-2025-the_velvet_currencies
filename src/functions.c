2e96ee2787fc1dc38815f2be8cffb03580a320f8
ArthurBloch <arthur.bloch-golliot@etu.estia.fr>
2025-06-13 15:29:03 +0200

    min_component V1

--- src/functions.c
+++ src/functions.c
@@ -37,6 +37,32 @@
 
 
 /* min component*/
-void min_component(char *image) {
-    
+void min_component(char *source_path, char color){
+    unsigned char *data;
+    int w;
+    int h;
+    int n;
+    read_image_data(source_path, &data, &w, &h, &n);
+    int x,y ;
+    int j, min = 255;
+    int min_pix = (0,0);
+
+    if (color == 'R') {
+        j = 0
+    }
+    if (color == 'G') {
+        j = 1
+    }
+    if (color == 'B') {
+        j = 2
+    }
+    for (y=0 ; y<h ; y++) {
+        for (x=0 ; x<w ; x++) {
+            if (data[(y*w+x)*n+j] < min) {
+                min = data[(y*w+x)*n+j];
+                min_pix(x,y);
+            }
+        }
+    }
+    return printf("min_component %d : (%d,%d)", color, min_pix);
 }
