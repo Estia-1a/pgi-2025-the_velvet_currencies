4c683207b7850781baf0f0e9c5249d9548ac8f4b
Yoann2003 <yoann.baudoin@etu.estia.fr>
2025-06-17 11:19:09 +0200

    color in red

--- src/features.h
+++ src/features.h
@@ -7,5 +7,6 @@ void helloWorld();
 void first_pixel(char *image);
 void dimension(char *source_path);
 void tenth_pixel(char *source_path);
-void max_component(char *image, char *source_path);
+void max_component(char *image);
+
 #endif
