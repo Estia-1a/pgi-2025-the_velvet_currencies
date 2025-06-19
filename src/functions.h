485db1d22726dc9831a9ab6bcc30d65d552322d4
Younes783 <younes.berrehili@etu.estia.fr>
2025-06-19 21:26:35 +0200

    Fin du code Color_desaturate

---
+++ src/functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void max_pixel(char *image_path);
+void min_component(char *image, char *color);
+void color_invert(char *image_path);
+void color_gray_luminance(char*image_path);
+void color_gray(char*image_path);
+void mirror_horizontal(char*image_path);
+void mirror_vertical(char*image_path);
+void mirror_total(char*image_path);
+void color_desaturate(char*image_path);
+#endif
