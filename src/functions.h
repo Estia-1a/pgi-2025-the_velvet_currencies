a0651e32a00c28e8f1dd70b17a86dc44ec7037ca
ArthurBloch <arthur.bloch-golliot@etu.estia.fr>
2025-06-19 23:02:26 +0200

    tests_V2

--- src/functions.h
+++ src/functions.h
@@ -1,13 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-void max_pixel(char *image_path);
-void min_component(char *image, char *color);
-void color_invert(char *image_path);
-void color_gray_luminance(char*image_path);
-void color_gray(char*image_path);
-void mirror_horizontal(char*image_path);
-void mirror_vertical(char*image_path);
-void mirror_total(char*image_path);
-void scale_crop(char *image_path, int xc, int yc, int lc, int Lc);
+
+
+
 #endif
